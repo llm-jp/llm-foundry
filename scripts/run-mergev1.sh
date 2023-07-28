f7074f1cdb234018b9035b1498e4f3b78534c36b
Taishi-N324 <taishiahiru5@gmail.com>
2023-07-28 17:01:16 +0900

    test for merge data

---
+++ scripts/run-mergev1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd /model/mosaicml/llm-foundry/scripts
+source venv/bin/activate
+
+readonly training_script="/model/mosaicml/llm-foundry/scripts/train/train.py"
+readonly yaml_base_dir="/model/mosaicml/llm-foundry/scripts/"
+
+
+function run() {
+    local world_size=$1
+    local rank=$2
+    local addr=$3
+    local port=$4
+    local params=$5
+
+    local training_script_args="$yaml_base_dir/mpt-13b-2node16gpu-merge-data.yaml"
+
+    composer \
+        --verbose \
+        --world_size $world_size \
+        --node_rank $rank \
+        --master_addr $addr \
+        --master_port $port \
+        $training_script \
+        $training_script_args \
+    data_local=v001_merge train_loader.dataset.split=train eval_interval=1 \
+    save_folder=merge-data-test
+}
+
+run $@
