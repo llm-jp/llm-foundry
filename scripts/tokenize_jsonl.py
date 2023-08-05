4fe941588739a3f3fe82270f993ac8c9edabbc07
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2023-08-05 15:31:03 +0900

    add tokenize_json.*

---
+++ scripts/tokenize_jsonl.py
@@ -0,0 +1,23 @@
+from array import array
+import gzip
+import json
+import sys
+
+from sentencepiece import SentencePieceProcessor
+
+
+def tokenize_jsonl(tokenizer, fin, fout, input_field="text"):
+    for line in fin:
+        r = json.loads(line)
+        text = r[input_field]
+        token_ids = tokenizer.encode(text)
+        buf = array("I")
+        buf.append(len(token_ids))
+        buf.extend(token_ids)
+        buf.tofile(fout)
+
+
+if __name__ == "__main__":
+    tokenizer = SentencePieceProcessor(model_file=sys.argv[1])
+    with gzip.open(sys.argv[2], "wb") as fout:
+        tokenize_jsonl(tokenizer, sys.stdin, fout)
---
+++ scripts/tokenize_jsonl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+srcdir=/data/data_v001_20230706
+dstdir=./tokenized/$1
+for dataset in code_stack ja_wiki en_wiki ja_cc en_pile
+do
+  echo `date "+%Y/%m/%d %H:%M:%S"` tokenizing $srcdir/$dataset.tar.gz
+  mkdir -p $dstdir/$dataset
+  tar zxf $srcdir/$dataset.tar.gz -O | parallel --pipe -j 32 --round --blocksize 10000000 python tokenize_jsonl.py $1 $dstdir/$dataset/'{%}.bin.gz'
+  echo `date "+%Y/%m/%d %H:%M:%S"` "done"
+done
