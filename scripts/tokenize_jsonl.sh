ce8e4c5b629e135cd82c4a31847864222092769f
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2023-08-06 13:12:20 +0900

    increase block size for en_pile

--- scripts/tokenize_jsonl.sh
+++ scripts/tokenize_jsonl.sh
@@ -8,6 +8,6 @@ for dataset in code_stack ja_wiki en_wiki ja_cc en_pile
 do
   echo `date "+%Y/%m/%d %H:%M:%S"` tokenizing $srcdir/$dataset.tar.gz
   mkdir -p $dstdir/$dataset
-  tar zxf $srcdir/$dataset.tar.gz -O | parallel --pipe -j 32 --round --blocksize 10000000 python tokenize_jsonl.py $1 $dstdir/$dataset/'{%}.bin.gz'
+  tar zxf $srcdir/$dataset.tar.gz -O | parallel --pipe -j 32 --round --blocksize 15000000 python tokenize_jsonl.py $1 $dstdir/$dataset/'{%}.bin.gz'
   echo `date "+%Y/%m/%d %H:%M:%S"` "done"
 done
