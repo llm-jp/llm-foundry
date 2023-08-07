4b3cad11efa872aeb6739ba8ce1ce29c41e32958
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2023-08-07 18:48:25 +0900

    improve log

--- scripts/tokenize_jsonl.sh
+++ scripts/tokenize_jsonl.sh
@@ -7,7 +7,7 @@ dstdir=./tokenized/$1
 mkdir -p $dstdir
 for dataset in code_stack ja_wiki en_wiki ja_cc en_pile
 do
-  echo `date "+%Y/%m/%d %H:%M:%S"` "$srcdir/$dataset.tar.gz -> $dstdir started" && \
+  echo `date "+%Y/%m/%d %H:%M:%S"` "$srcdir/$dataset.tar.gz -> $dstdir/$dataset.bin.gz started" && \
   pigz -d -c $srcdir/$dataset.tar.gz | tar xf - -O | parallel --pipe -j 16 --round -u --blocksize 15000000 python tokenize_jsonl.py $1 | pigz -c > $dstdir/$dataset.bin.gz && \
-  echo `date "+%Y/%m/%d %H:%M:%S"` "$srcdir/$dataset.tar.gz -> $dstdir done" &
+  echo `date "+%Y/%m/%d %H:%M:%S"` "$srcdir/$dataset.tar.gz -> $dstdir/$dataset.bin.gz done" &
 done
